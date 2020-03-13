dd9b4acea78b04f1fa1320d7a670ae4d281eb547
Karl <git@karlneugebauer.com>
2020-03-13 19:39:14 +0100

    more Vector3 stuff

---
+++ scripts/Vector3.js
@@ -0,0 +1,62 @@
+const Vector3 = class vector3 {
+  constructor (x = 0, y = 0, z = 0) {
+    this.x = x
+    this.y = y
+    this.z = z
+  }
+
+  /* ---------- static ---------- */
+  static add (left, right) {
+    if (typeof right === "number") {
+      return new Vector3(left.x + right, left.y + right, left.z + right)
+    }
+    return new Vector3(left.x + right.x, left.y + right.x, left.z + right.z)
+  }
+
+  static subtract (left, right) {
+    if (typeof right === "number") {
+      return new Vector3(left.x - right, left.y - right, left.z - right)
+    }
+    return new Vector3(left.x - right.x, left.y - right.x, left.z - right.z)
+  }
+
+  static multiply (left, right) {
+    if (typeof right === "number") {
+      return new Vector3(left.x * right, left.y * right, left.z * right)
+    }
+    return new Vector3(left.x * right.x, left.y * right.x, left.z * right.z)
+  }
+
+  static divide (left, right) {
+    if (typeof right === "number") {
+      return new Vector3(left.x / right, left.y / right, left.z / right)
+    }
+    return new Vector3(left.x / right.x, left.y / right.x, left.z / right.z)
+  }
+
+  static negate (self) {
+    return new Vector3(-self.x, -self.y, -self.z)
+  }
+
+  static equal (left, right) {
+    return left.x === right.x && left.y === right.y && left.z === right.z
+  }
+
+  static lerp (left, right, t) {
+    return Vector3.add(Vector3.multiply(left, t), Vector3.multiply(right, 1.0 - t))
+  }
+
+  /* ---------- self ---------- */
+  negate (self) {
+    return new Vector3(-self.x, -self.y, -self.z)
+  }
+
+  equal (other) {
+    return this.x === other.x && this.y === other.y && this.z === other.z
+  }
+
+  /* ---------- get ---------- */
+  get magnitude () {
+    return this.x * this.x + this.y * this.y + this.z * this.z
+  }
+}
--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,17 +1,10 @@
-const Vector3 = class vector3 {
-  constructor (x, y, z) {
-    this.x = x
-    this.y = y
-    this.z = z
-  }
-}
 
 const M_PI_4 = Math.PI / 4
 const timeStep = 1.0 / 60
 const rangeSearchErrorMax = 0.001 // ratio * distance
 const rangeSearchAngleConvergance = 0.000025
 const gravityABS = 9.8066
-const gravityAccel = new Vector3(0, 0, -1 * gravityABS)
+const gravityAccl = new Vector3(0, 0, -1 * gravityABS)
 
 /**
  *
@@ -26,7 +19,31 @@ const gravityAccel = new Vector3(0, 0, -1 * gravityABS)
  * @returns {{finalPosX: number, finalPosY: number, currentTime: number}}
  */
 function simulateShot (fireAngleRad, muzzleVelocity, heightOfTarget, crossWind, tailWind, temperature, airDensity, airFriction) {
-  return {finalPosX: 0, finalPosY: 0, currentTime: 0}
+  // returns: dist traveled to the side (crosswind), dist traveled foward (headwind), time of flight
+  // note: if shot never reaches height of target, then results are undefined (use negative)
+  const kCoefficient = -1.0 * airDensity * airFriction
+  const powderEffects = (airFriction) ? ((temperature + 273.13) / 288.13 - 1) / 40 + 1 : 1.0
+
+  let currentTime = 0
+  let currentPosition = new vector3(0, 0, 0)
+  let lastPosition = new vector3(currentPosition.x, currentPosition.y, currentPosition.z)
+  let currentVelocity = new vector3(0, powderEffects * muzzleVelocity * Math.cos(fireAngleRad), powderEffects * muzzleVelocity * Math.sin(fireAngleRad))
+  const wind = new vector3(crossWind, tailWind, 0)
+
+  while ((currentVelocity.z > 0) || (currentPosition.z >= heightOfTarget)) {
+    lastPosition = currentPosition
+    let apparentWind = Vector3.subtract(wind, currentVelocity)
+    let changeInVelocity = Vector3.add(gravityAccl, Vector3.multiply(apparentWind, Vector3.multiply(kCoefficient, apparentWind.magnitude)))
+    changeInVelocity = gravityAccl + apparentWind * (kCoefficient * apparentWind.magnitude())
+    currentVelocity += changeInVelocity * timeStep
+    currentPosition += currentVelocity * timeStep
+    currentTime += timeStep
+  }
+
+  const lastCurrentRatio = (heightOfTarget - currentPosition.z) / (lastPosition.z - currentPosition.z)
+  let finalPos = Vector3.lerp(lastPosition, currentPosition, lastCurrentRatio)
+
+  return {finalPosX: finalPos.x, finalPosY: finalPos.y, currentTime}
 }
 
 /**
@@ -44,7 +61,7 @@ function findMaxAngle (muzzleVelocity, airFriction) {
   let bestAngle = M_PI_4
   let bestDistance = -1
   for (let testAngle = M_PI_4; testAngle > 0; testAngle -= (M_PI_4 / 100.0)) {
-    let {_ignore1, testResultDist, _ignore3 } = simulateShot(testAngle, muzzleVelocity, 0, 0, 0, 15, 1, airFriction)
+    let {_ignore1, testResultDist, _ignore3} = simulateShot(testAngle, muzzleVelocity, 0, 0, 0, 15, 1, airFriction)
     if (testResultDist > bestDistance) {
       bestAngle = testAngle
       bestDistance = testResultDist
@@ -64,16 +81,16 @@ function findMaxAngle (muzzleVelocity, airFriction) {
  * @param highArc
  * @returns {{resultDistance: number, resultTime: number, currentElevation: number}}
  */
-function simulatefindSolution (rangeToHit, heightToHit, muzzleVelocity, airFriction, minElev, maxElev, highArc) {
+function simulateFindSolution (rangeToHit, heightToHit, muzzleVelocity, airFriction, minElev, maxElev, highArc) {
   // returns: actual distance traveled, elevation, time of flight
   let searchMin = minElev
   let searchMax = maxElev
 
   if (!airFriction) {
     // can do trivial ballistics physics to get angle, could compute tof as well, but run through sim once to ensure consistancy (uses positive value of g)
-    let radicand = Math.pow(muzzleVelocity, 4) - gravityABS * (gravityABS * Math.pow(rangeToHit, 2) + 2 * heightToHit * pow(muzzleVelocity, 2))
+    let radicand = Math.pow(muzzleVelocity, 4) - gravityABS * (gravityABS * Math.pow(rangeToHit, 2) + 2 * heightToHit * Math.pow(muzzleVelocity, 2))
     if ((!rangeToHit) || (radicand < 0)) { // radican't
-      return { resultDistance: -1, currentElevation: -1, resultTime: -1 }
+      return {resultDistance: -1, currentElevation: -1, resultTime: -1}
     }
     radicand = Math.sqrt(radicand)
     let angleRoot = Math.atan((Math.pow(muzzleVelocity, 2) + radicand) / (gravityABS * rangeToHit))
@@ -81,10 +98,10 @@ function simulatefindSolution (rangeToHit, heightToHit, muzzleVelocity, airFrict
       angleRoot = Math.atan((Math.pow(muzzleVelocity, 2) - radicand) / (gravityABS * rangeToHit))
     }
     if ((angleRoot > maxElev) || (angleRoot < minElev)) {
-      return { resultDistance: -1, currentElevation: -1, resultTime: -1 }
+      return {resultDistance: -1, currentElevation: -1, resultTime: -1}
     }
     const tof = rangeToHit / (muzzleVelocity * Math.cos(angleRoot))
-    return { resultDistance: rangeToHit, currentElevation: angleRoot, resultTime: tof }
+    return {resultDistance: rangeToHit, currentElevation: angleRoot, resultTime: tof}
   }
 
   let numberOfAttempts = 0
@@ -93,7 +110,9 @@ function simulatefindSolution (rangeToHit, heightToHit, muzzleVelocity, airFrict
   let currentError = 9999
   let currentElevation = -1
   do {
-    if (numberOfAttempts++ > 50) { break } // for safetey, min/max should converge long before
+    if (numberOfAttempts++ > 50) {
+      break
+    } // for safetey, min/max should converge long before
     currentElevation = (searchMin + searchMax) / 2.0
     let {_ignore1, resultDistance, resultTime} = simulateShot(currentElevation, muzzleVelocity, heightToHit, 0, 0, 15, 1, airFriction)
     currentError = rangeToHit - resultDistance
@@ -108,9 +127,9 @@ function simulatefindSolution (rangeToHit, heightToHit, muzzleVelocity, airFrict
   // printf("[%f, %f] Actuall [%f] err [%f of %f]\n", _rangeToHit, _heightToHit, resultDistance, currentError, (_rangeToHit * rangeSearchErrorMax * (_highArc ? 1.0 : 2.0)));
   // On some low angle shots, it will really struggle to converge because of precision issues
   if ((Math.abs(currentError) > (rangeToHit * rangeSearchErrorMax * (highArc ? 1.0 : 2.0)))) {
-    return { resultDistance: -1, currentElevation: -1, resultTime: -1 }
+    return {resultDistance: -1, currentElevation: -1, resultTime: -1}
   }
-  return { resultDistance, currentElevation, resultTime }
+  return {resultDistance, currentElevation, resultTime}
 }
 
 
